93a767f4d1f87d14e5bc459784816d77079b1050
Per Fredelius <per.fredelius@findwise.com>
2015-05-14 19:17:14 +0200

    clustering fix

--- cluster.js
+++ cluster.js
@@ -4,26 +4,23 @@ var enableClustering = true;
 if(enableClustering){
   // Connect to the cluster with a MongoDB URL. Better if it's a replica set
   var connectOptions;
-  Cluster.connect("mongodb://127.0.0.1:3001/meteor", connectOptions = {
+  // 10.47.12.42  // 127.0.0.1
+  Cluster.connect("mongodb://localhost/discover"/*, connectOptions = {
     // Value of 0 to 1, mentioning which portion of requestes to process here or proxy
     // If 1, all the requests allocated to this host will get processed
     // If 0.5 half of the requsted allocated to this host will get processed, others will get proxied
     // If 0, only do proxying 
     selfWeight: 1 // optional
-  });
+  }*/);
 
   // Register a service to the cluster
   var options;
-  Cluster.register("myMeteorService", options = {
-    endpoint: "http://10.47.12.42:3000"//,
-    //balancer: "balancer URL, if this is a balancer", // optional
-    //uiService: "service to proxy UI" // (optional) read to the end for more info
-  });
+  Cluster.register("myMeteorService");
 
   // Expose services to the public
-  Cluster.allowPublicAccess(["myMeteorService"]);
+  //Cluster.allowPublicAccess(["myMeteorService"]);
 
   // Discover a DDP connection
   // > This is available on both the client and the server
-  Cluster.discoverConnection("myMeteorService");
+  //Cluster.discoverConnection("myMeteorService");
 }
